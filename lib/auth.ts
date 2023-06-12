1b67195dc8fc0c2f8e6d9ce4c48d0151b0cd02eb
Zhikrullah IGBALAJOBI <zalajobi@bask.health>
2023-06-12 21:25:42 +0100

    site route authentication refactor

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,7 +3,6 @@ import {verifyJSONToken} from "../helpers/utils";
 
 export const verifyUserPermission = async (token:string, roleRequired:string[]) => {
   const { role } = await <JWTDataProps><unknown>verifyJSONToken(token)
-  console.log(role)
 
   for (const item of roleRequired) {
     if (item === role)
