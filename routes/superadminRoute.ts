f2b8ccdae88628f40ca6de8f7c0d14beee4f232d
Zhikrullah IGBALAJOBI <zalajobi@bask.health>
2023-06-13 16:04:15 +0100

    superadmin loginn validation and jwt token generation

--- routes/superadminRoute.ts
+++ routes/superadminRoute.ts
@@ -107,24 +107,20 @@ superadminRouter.post('/super-admin/auth/login', async (req, res) => {
   let responseMessage = 'Incorrect Credentials', jwtSignData = null, success = false
 
   try {
-    console.log(req.body.email)
-    console.log(req.body.password)
     const admin = await getSuperadminLoginData(req.body.email)
 
-    console.log(admin)
+    if (validatePassword(req.body.password, admin?.password ?? '')) {
+      const jwtData = {
+        id: admin?.id as string,
+        email: admin?.email as string,
+        role: admin?.role as string
+      }
 
-    // if (validatePassword(req.body.password, admin?.password ?? '')) {
-    //   const jwtData = {
-    //     id: admin?.id ?? '',
-    //     email: admin?.email ?? '',
-    //     role: 'SUPER_ADMIN'
-    //   }
-    //
-    //   jwtSignData = generateJSONTokenCredentials(jwtData, Math.floor(Date.now() / 1000) + (60 * 360))
-    //   responseMessage= 'Login Successful'
-    //   success = true
-    // }
-    //
+      jwtSignData = generateJSONTokenCredentials(jwtData, Math.floor(Date.now() / 1000) + (60 * 360))
+      responseMessage= 'Login Successful'
+      success = true
+    }
+
     return JsonResponse(res, responseMessage, success, {
       token: jwtSignData
     }, 200)
