094f38444c4ac11a1c8be5f58b625e353a322d81
Zhikrullah IGBALAJOBI <zalajobi@bask.health>
2023-06-18 00:33:29 +0100

    configure husky and prettier git precommit hook

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,22 @@
+// module.exports = {
+//   trailingComma: "es5",
+//   tabWidth: 2,
+//   semi: true,
+//   singleQuote: true,
+//   doubleQuote: false,
+//   bracketSpacing: true,
+//   bracketSameLine: true
+//   printWidth: 80,
+//   plugins: ["prettier-plugin-tailwindcss"],
+//   pluginSearchDirs: false,
+// }
+
 module.exports = {
-  trailingComma: "es5",
+  trailingComma: 'es5',
+  semi: true,
   tabWidth: 2,
-  semi: false,
-  singleQuote: false,
-  doubleQuote: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: true,
   printWidth: 80,
-  plugins: ["prettier-plugin-tailwindcss"],
-  pluginSearchDirs: false,
-}
+};
