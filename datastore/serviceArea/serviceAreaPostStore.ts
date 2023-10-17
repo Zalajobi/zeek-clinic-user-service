123c6ad4a1c390b92048cee63359dc74aa5a8774
Zhikrullah IGBALAJOBI <zalajobi@bask.health>
2023-10-17 10:27:22 +0100

    project file cleanup

--- datastore/serviceArea/serviceAreaPostStore.ts
+++ datastore/serviceArea/serviceAreaPostStore.ts
@@ -1,18 +1,32 @@
 import { CreateServiceAreaDataProps } from '@typeorm/objectsTypes/serviceAreaObjectType';
 import { serviceAreaRepo } from '@typeorm/repositories/serviceAreaRepository';
 import { Servicearea } from '@typeorm/entity/servicearea';
+import { DefaultJsonResponse } from '@util/responses';
 
 export const createServiceArea = async (data: CreateServiceAreaDataProps) => {
   const serviceAreaRepository = serviceAreaRepo();
 
-  const newServiceArea = await serviceAreaRepository.save(
-    new Servicearea(data)
-  );
+  // If Service Area already exists in the same site, do no create
+  const isUnique = await serviceAreaRepository
+    .createQueryBuilder('unit')
+    .where('LOWER(unit.name) LIKE LOWER(:name)', {
+      name: data.name,
+    })
+    .andWhere('unit.siteId = :siteId', {
+      siteId: data?.siteId,
+    })
+    .getCount();
 
-  return {
-    success: !!newServiceArea,
-    message: newServiceArea
+  if (isUnique >= 1)
+    return DefaultJsonResponse('Unit with name already exists', null, false);
+
+  const units = await serviceAreaRepository.save(new Servicearea(data));
+
+  return DefaultJsonResponse(
+    units
       ? 'New Service Area Created'
-      : 'Something Happened. Error happened While Creating Service Area',
-  };
+      : 'Something happened. Error happened while creating Department',
+    null,
+    !!units
+  );
 };
--- routes/serviceArea/index.ts
+++ routes/serviceArea/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import serviceAreaGetRequest from '@routes/serviceArea/serviceAreaGetRequestHandler';
 import serviceAreaPutRequest from '@routes/serviceArea/serviceAreaPutRequestHandler';
+import serviceAreaPostRequest from '@routes/serviceArea/serviceAreaPostRequestHandler';
 
 const serviceAreaRouter = Router();
 
 serviceAreaRouter.use('/', serviceAreaGetRequest);
 serviceAreaRouter.use('/', serviceAreaPutRequest);
+serviceAreaRouter.use('/', serviceAreaPostRequest);
 
 export default serviceAreaRouter;
