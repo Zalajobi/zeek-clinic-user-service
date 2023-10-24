ba94d3c56affbbac65d6a23db11900be950a90d9
Zhikrullah IGBALAJOBI <zalajobi@bask.health>
2023-10-24 19:14:17 +0100

    update role data by roleId API

--- routes/role/index.ts
+++ routes/role/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import rolePostRequest from '@routes/role/rolePostRequestHandler';
 import roleGetRequest from '@routes/role/roleGetRequestHandler';
+import rolePutRequest from '@routes/role/rolePutRequestHandler';
 
 const roleRouter = Router();
 roleRouter.use('/', rolePostRequest);
 roleRouter.use('/', roleGetRequest);
+roleRouter.use('/', rolePutRequest);
 
 export default roleRouter;
