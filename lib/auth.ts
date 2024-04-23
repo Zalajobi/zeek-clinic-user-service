e145ae1539426d4f350cd3b34636cf439c1dba56
Igbalajobi Zhikrullah <zalajobi@gmail.com>
2024-04-23 22:15:18 +0100

    prettier

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,17 +3,9 @@ import { JWTDataProps } from '@typeDesc/jwt';
 
 export const verifyUserPermission = (token: string, roleRequired: string[]) => {
   const tokenUser = <JWTDataProps>(<unknown>verifyJSONToken(token));
-  console.log({
-    roleRequired,
-  });
 
   for (const item of roleRequired) {
-    if (item === tokenUser?.role) {
-      console.log({
-        role: tokenUser?.role,
-      });
-      return tokenUser;
-    }
+    if (item === tokenUser?.role) return tokenUser;
   }
   return null;
 };
