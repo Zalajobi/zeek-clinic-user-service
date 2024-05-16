4f23ae3b504a5108b8909e03ca6e1dadcdfdefc5
Igbalajobi Zhikrullah <zalajobi@gmail.com>
2024-05-16 02:29:11 +0100

    search units

--- datastore/unit/unitGetStore.ts
+++ datastore/unit/unitGetStore.ts
@@ -1,5 +1,8 @@
 import { unitRepo } from '@typeorm/repositories/unitRepositories';
 import { DefaultJsonResponse } from '@util/responses';
+import { searchUnitRequestSchema } from '@lib/schemas/unitSchemas';
+import { z } from 'zod';
+import { extractPerPageAndPage } from '@helpers/utils';
 
 // Get Unit in a site by the SiteId and their provider count
 export const fetchFilteredUnitData = async (
@@ -74,4 +77,73 @@ export const getUnitDataBySiteID = async (siteId: string) => {
       name: true,
     },
   });
+};
+
+export const getSearchUnitData = async (
+  requestBody: z.infer<typeof searchUnitRequestSchema>
+) => {
+  const unitRepository = unitRepo();
+
+  const { page, perPage } = extractPerPageAndPage(
+    requestBody.endRow,
+    requestBody.startRow
+  );
+
+  const unitQuery = unitRepository.createQueryBuilder('dept').orderBy({
+    [`${requestBody.sortModel.colId}`]:
+      requestBody.sortModel.sort === 'asc' ? 'ASC' : 'DESC',
+  });
+
+  if (requestBody.siteId) {
+    unitQuery.where('dept.siteId = :siteId', {
+      siteId: requestBody.siteId,
+    });
+  }
+
+  if (requestBody.id) {
+    unitQuery.where('dept.id = :id', {
+      id: requestBody.id,
+    });
+  }
+
+  if (requestBody.name) {
+    unitQuery.where('dept.name = :name', {
+      name: requestBody.name,
+    });
+  }
+
+  // if (requestBody.total_beds) {
+  //   unitQuery.where('dept.total_beds >= :total_beds', {
+  //     total_beds: requestBody.total_beds,
+  //   });
+  // }
+  //
+  // if (requestBody.occupied_beds) {
+  //   unitQuery.where('dept.occupied_beds >= :occupied_beds', {
+  //     occupied_beds: requestBody.occupied_beds,
+  //   });
+  // }
+
+  if (requestBody?.range && requestBody.range.from) {
+    unitQuery.andWhere('dept.created_at > :fromDate', {
+      fromDate: requestBody.range.from,
+    });
+  }
+
+  if (requestBody?.range && requestBody.range.to) {
+    unitQuery.andWhere('dept.created_at < :toDate', {
+      toDate: requestBody.range.to,
+    });
+  }
+
+  if (requestBody.search && requestBody.searchKey) {
+    unitQuery.andWhere(`LOWER(dept.${requestBody.searchKey}) LIKE :search`, {
+      search: `%${requestBody.search.toLowerCase()}%`,
+    });
+  }
+
+  return await unitQuery
+    .skip(perPage * page)
+    .take(perPage)
+    .getManyAndCount();
 };
