786728a5d13b647a56811ee91e6bb14c0aa633fe
Igbalajobi Zhikrullah <zalajobi@gmail.com>
2024-06-02 09:38:36 +0100

    get providers site distribution

--- lib/schemas/enums.ts
+++ lib/schemas/enums.ts
@@ -35,9 +35,11 @@ export const distributionSchema = z
     if (typeof val === 'string') return val.toLowerCase();
 
     return val;
-  }, z.enum(['department', 'unit', 'service area']))
+  }, z.enum(['department', 'unit', 'service area', 'role']))
   .transform((val) => {
     if (val === 'service area') return 'serviceArea';
+
+    if (val === 'role') return 'primaryRole';
 
     return val;
   });
